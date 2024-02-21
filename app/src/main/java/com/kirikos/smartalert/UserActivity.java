f093fcb37b5960d7996e6f5cdcfaa1fa32d366d8
kirikosd <kirikosdoumanis@gmail.com>
2024-02-21 18:48:06 +0200

    Modified users layout

--- app/src/main/java/com/kirikos/smartalert/UserActivity.java
+++ app/src/main/java/com/kirikos/smartalert/UserActivity.java
@@ -2,7 +2,11 @@ package com.kirikos.smartalert;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class UserActivity extends AppCompatActivity {
 
@@ -10,5 +14,21 @@ public class UserActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user);
+
+        //get the spinner from the xml.
+        Spinner dropdown = findViewById(R.id.spinner1);
+        //create a list of items for the spinner.
+        String[] items = new String[]{"Πλημμύρα", "Πυρκαγιά", "Σεισμός"};
+        //create an adapter to describe how the items are displayed, adapters are used in several places in android.
+        //There are multiple variations of this, but this is the basic variant.
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
+        //set the spinners adapter to the previously created one.
+        dropdown.setAdapter(adapter);
+    }
+    public void addImage() {
+        // add image code
+    }
+    public void submit() {
+        // submit case code
     }
 }
