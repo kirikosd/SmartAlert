6a3a04baff757858d5117b4eb869b5bea5b05b24
kirikosd <kirikosdoumanis@gmail.com>
2024-02-22 18:40:51 +0200

    Added gps method

--- app/src/main/java/com/kirikos/smartalert/UserActivity.java
+++ app/src/main/java/com/kirikos/smartalert/UserActivity.java
@@ -1,19 +1,27 @@
 package com.kirikos.smartalert;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import android.Manifest;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
 public class UserActivity extends AppCompatActivity {
+    LocationManager locationManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user);
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
 
         //get the spinner from the xml.
         Spinner dropdown = findViewById(R.id.spinner1);
@@ -30,5 +38,17 @@ public class UserActivity extends AppCompatActivity {
     }
     public void submit() {
         // submit case code
+    }
+    public void gps(View view) {
+        if (ActivityCompat.checkSelfPermission(this,
+                android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED)
+        {
+            ActivityCompat.requestPermissions(
+                    this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION},123);
+            return;
+        }
+
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, (LocationListener) this);
+        //locationManager.removeUpdates(this);
     }
 }
