96aad6199f98c7d8ca10989e8cb4b76699916b58
kirikosd <kirikosdoumanis@gmail.com>
2024-03-02 19:35:53 +0200

    Updated SignIn and SignUp activities

--- app/src/main/java/com/kirikos/smartalert/auth/SignInActivity.java
+++ app/src/main/java/com/kirikos/smartalert/auth/SignInActivity.java
@@ -1,19 +1,68 @@
 package com.kirikos.smartalert.auth;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.kirikos.smartalert.R;
 import com.kirikos.smartalert.employee.InspectCasesActivity;
 import com.kirikos.smartalert.user.UserHomePageActivity;
 
 public class SignInActivity extends AppCompatActivity {
-
+    private FirebaseAuth mAuth;
+    private String email;
+    private String password;
+    private EditText emailEditText;
+    private EditText passwordEditText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+
+        emailEditText = findViewById(R.id.emailEditText);
+        passwordEditText = findViewById(R.id.passwordEditText);
+
+        mAuth = FirebaseAuth.getInstance();
+    }
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+            currentUser.reload();
+        }
+    }
+    public void signIn(View view) {
+        email = emailEditText.getText().toString();
+        password = passwordEditText.getText().toString();
+
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            Toast.makeText(getApplicationContext(), "Επιτυχής είσοδος", Toast.LENGTH_LONG).show();
+
+//                            updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Toast.makeText(getApplicationContext(), "Η είσοδος απέτυχε", Toast.LENGTH_LONG).show();
+//                            updateUI(null);
+                        }
+                    }
+                });
     }
     public void goToUser(View view) {
         Intent intent = new Intent(this, UserHomePageActivity.class);
--- app/src/main/java/com/kirikos/smartalert/auth/SignUpActivity.java
+++ app/src/main/java/com/kirikos/smartalert/auth/SignUpActivity.java
@@ -1,19 +1,33 @@
 package com.kirikos.smartalert.auth;
 
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.kirikos.smartalert.R;
 
 public class SignUpActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
+    private String email;
+    private String password;
+    private EditText emailEditText;
+    private EditText passwordEditText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        emailEditText = findViewById(R.id.emailEditText);
+        passwordEditText = findViewById(R.id.passwordEditText);
 
         mAuth = FirebaseAuth.getInstance();
     }
@@ -25,5 +39,27 @@ public class SignUpActivity extends AppCompatActivity {
         if(currentUser != null){
             currentUser.reload();
         }
+    }
+
+    public void signUp(View view){
+        email = emailEditText.getText().toString();
+        password = passwordEditText.getText().toString();
+
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            Toast.makeText(getApplicationContext(), "Η εγγραφή ολοκληρώθηκε με επιτυχία", Toast.LENGTH_LONG).show();
+//                            updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Toast.makeText(getApplicationContext(), "Η εγγραφή απέτυχε", Toast.LENGTH_LONG).show();
+//                            updateUI(null);
+                        }
+                    }
+                });
     }
 }
